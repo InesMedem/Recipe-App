3c524e4a7d2f601c731f5169facf7cd8c4aa693e
InesMedem <inesmedem93@gmail.com>
2024-06-04 15:49:17 +0200

    import corrections, index modes to src

--- Server/src/routes/users.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-
-const router = express.Router();
-
-export { router as serRouter };
---
+++ src/routes/users.js
@@ -0,0 +1,16 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const router = express.Router();
+// import { UserModel } from "../models/Users.js";
+
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username: username });
+  res.json(user);
+});
+
+router.post("/login");
+
+export { router as userRouter };
