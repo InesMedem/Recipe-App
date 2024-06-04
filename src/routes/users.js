c9b9f66e7183f13c2bac3e0d1113316f5d3c0792
InesMedem <inesmedem93@gmail.com>
2024-06-04 16:49:44 +0200

    pasword hashing done

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,24 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import { UserModel } from "../models/user.js";
 
 const router = express.Router();
-import { UserModel } from "../models/user.js";
 
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
   const user = await UserModel.findOne({ username: username });
-  res.json(user);
+
+  if (user) {
+    return res.json({ message: "user already exists!" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new UserModel({ username, password: hashedPassword });
+  await newUser.save();
+
+  res.json({ message: "user registeres Sucesfully!" });
 });
 
 router.post("/login");
