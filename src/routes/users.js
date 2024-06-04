fd120cf9580f04c422ca8bac6d7798db625ff060
InesMedem <inesmedem93@gmail.com>
2024-06-04 17:17:00 +0200

    Login & register implemented

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,9 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { UserModel } from "../models/user.js";
+import dotenv from "dotenv";
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const router = express.Router();
 
@@ -21,6 +24,19 @@ router.post("/register", async (req, res) => {
   res.json({ message: "user registeres Sucesfully!" });
 });
 
-router.post("/login");
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username });
+
+  if (!user) {
+    return res.json({ message: "User doesn't exist" });
+  }
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.json({ message: "Username or Password Is Incorrect!" });
+  }
+  const token = jwt.sign({ id: user._id }, "JWT_SECRET");
+  res.json({ token, userID: user._id });
+});
 
 export { router as userRouter };
